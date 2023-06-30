866062105ccd298718e9ea3008741b2e6b423be1
ivo.souza <ivo.souza@varixx.com.br>
2023-06-30 14:33:00 -0300

    fix

--- user.js
+++ user.js
@@ -21,10 +21,12 @@ socket.on("rauth", function(e){
     }
     else 
     {
+        $("#popups").html("<div id='warning' class='bg-warning mt-5 text-center rounded shadow text-white' style='display:none; width: 250px; height: 150px; font-size: 1.5rem;'><p>Login ou senha incorreto!!</p></div>")
         $("#warning").fadeIn("fast");
         setTimeout(() => {
             $("#warning").fadeOut("fast");
         }, 3000);
+        
     }
 })
 
@@ -39,28 +41,44 @@ $("#register").click(function(){
 socket.on("registered", function(e){
     switch (e){
         case '1': 
+            $("#popups").html("<div id='success' class='bg-success mt-5 text-center rounded shadow text-white' style='display:none; width: 250px; height: 150px; font-size: 1.5rem;'><p>Criado com Sucesso!!</p></div>")
             $("#success").fadeIn("fast");
             setTimeout(() => {
                 $("#success").fadeOut("fast");
             }, 3000);
+            setTimeout(() => {
+                $("#popups").html("");
+            }, 4000);
             break;
         case '2':
+            $("#popups").html("<div id='fail' class='bg-danger mt-5 text-center rounded shadow text-white' style='display:none; width: 250px; height: 150px; font-size: 1.5rem;'><p>Preencha todos os campos!!</p></div>")
             $("#fail").fadeIn("fast");
             setTimeout(() => {
                 $("#fail").fadeOut("fast");
             }, 3000);
+            setTimeout(() => {
+                $("#popups").html("");
+            }, 4000);
             break;
         case '3':
+            $("#popups").html("<div id='user-fail' class='bg-danger mt-5 text-center rounded shadow text-white' style='display:none; width: 250px; height: 150px; font-size: 1.5rem;'><p>Usuario ja existente!!</p></div>")
             $("#user-fail").fadeIn("fast");
             setTimeout(() => {
                 $("#user-fail").fadeOut("fast");
             }, 3000);
+            setTimeout(() => {
+                $("#popups").html("");
+            }, 4000);
             break;
         case '4':
+            $("#popups").html("<div id='nick-fail' class='bg-danger mt-5 text-center rounded shadow text-white' style='display:none; width: 250px; height: 150px; font-size: 1.5rem;'><p>Nick ja existente!!</p></div>")
             $("#nick-fail").fadeIn("fast");
             setTimeout(() => {
                 $("#nick-fail").fadeOut("fast");
             }, 3000);
+            setTimeout(() => {
+                $("#popups").html("");
+            }, 4000);
             break;
     }
 })
